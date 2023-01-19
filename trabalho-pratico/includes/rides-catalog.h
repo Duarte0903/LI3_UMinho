180ff9e6bb631f201ecabb1be6779c0aa151559b
João Lopes <jp.lopes060303@gmail.com>
2023-01-19 17:53:45 +0000

    finished query 7

--- trabalho-pratico/includes/rides-catalog.h
+++ trabalho-pratico/includes/rides-catalog.h
@@ -11,11 +11,15 @@ void sort_rides_by_date(Rides_Catalog catalog);
 
 void sort_rides_by_city(Rides_Catalog catalog);
 
+void sort_rides_by_city_and_driver_id(Rides_Catalog catalog);
+
 char *get_q4(char *city, Rides_Catalog catalog);
 
 char *get_q5(unsigned short start_date, unsigned short end_date, Rides_Catalog catalog);
 
 char *get_q6(char *city, unsigned short start_date, unsigned short end_date, Rides_Catalog catalog);
+
+char *get_q7(unsigned short output_number, char *city, va_list args);
 
 void free_rides_catalog(Rides_Catalog catalog);
 
--- trabalho-pratico/src/queries.c
+++ trabalho-pratico/src/queries.c
@@ -10,7 +10,8 @@
 #include "../includes/date.h"
 #include "../includes/utils.h"
 
-void print_q1(FILE *output_file, char **fields, va_list args) {
+void print_q1(FILE *output_file, char **fields, va_list args)
+{
     Users_Catalog users_catalog = va_arg(args, Users_Catalog);
     Drivers_Catalog drivers_catalog = va_arg(args, Drivers_Catalog);
     char *output = NULL;
@@ -22,13 +23,15 @@ void print_q1(FILE *output_file, char **fields, va_list args) {
     else
         output = get_user_q1(id, users_catalog);
 
-    if (output) {
+    if (output)
+    {
         fprintf(output_file, "%s\n", output); // Optimize to fwrite?
         free(output);
     }
 }
 
-void print_q2(FILE *output_file, char **fields, va_list args) {
+void print_q2(FILE *output_file, char **fields, va_list args)
+{
     (void)va_arg(args, Users_Catalog);
     Drivers_Catalog drivers_catalog = va_arg(args, Drivers_Catalog);
     char *output = NULL;
@@ -40,10 +43,12 @@ void print_q2(FILE *output_file, char **fields, va_list args) {
 
     sort_drivers_by_average_rating(drivers_catalog); // otimizar para inactive accounts e com flag
 
-    while (i < n) {
+    while (i < n)
+    {
         output = get_q2(index, drivers_catalog);
 
-        if (output) {
+        if (output)
+        {
             fprintf(output_file, "%s\n", output); // Optimize to fwrite?
             i++;
             free(output);
@@ -53,7 +58,8 @@ void print_q2(FILE *output_file, char **fields, va_list args) {
     }
 }
 
-void print_q3(FILE *output_file, char **fields, va_list args) {
+void print_q3(FILE *output_file, char **fields, va_list args)
+{
     Users_Catalog users_catalog = va_arg(args, Users_Catalog);
     char *output = NULL;
     char *n_users = fields[1];
@@ -64,10 +70,12 @@ void print_q3(FILE *output_file, char **fields, va_list args) {
 
     sort_users_by_distance(users_catalog); // otimizar para ianctive accounts e com flag
 
-    while (i < n) {
+    while (i < n)
+    {
         output = get_q3(index, users_catalog);
 
-        if (output) {
+        if (output)
+        {
             fprintf(output_file, "%s\n", output); // Optimize to fwrite?
             i++;
             free(output);
@@ -77,7 +85,8 @@ void print_q3(FILE *output_file, char **fields, va_list args) {
     }
 }
 
-void print_q4(FILE *output_file, char **fields, va_list args) {
+void print_q4(FILE *output_file, char **fields, va_list args)
+{
     (void)va_arg(args, Users_Catalog);
     (void)va_arg(args, Drivers_Catalog);
     Rides_Catalog rides_catalog = va_arg(args, Rides_Catalog);
@@ -88,13 +97,15 @@ void print_q4(FILE *output_file, char **fields, va_list args) {
     sort_rides_by_city(rides_catalog);
     output = get_q4(city, rides_catalog);
 
-    if (output) {
+    if (output)
+    {
         fprintf(output_file, "%s\n", output);
         free(output);
     }
 }
 
-void print_q5(FILE *output_file, char **fields, va_list args) {
+void print_q5(FILE *output_file, char **fields, va_list args)
+{
     (void)va_arg(args, Users_Catalog);
     (void)va_arg(args, Drivers_Catalog);
     Rides_Catalog rides_catalog = va_arg(args, Rides_Catalog);
@@ -110,16 +121,18 @@ void print_q5(FILE *output_file, char **fields, va_list args) {
         return;
 
     sort_rides_by_date(rides_catalog);
-    
+
     output = get_q5(start_date, end_date, rides_catalog);
 
-    if (output) {
+    if (output)
+    {
         fprintf(output_file, "%s\n", output);
         free(output);
     }
 }
 
-void print_q6(FILE *output_file, char **fields, va_list args) {
+void print_q6(FILE *output_file, char **fields, va_list args)
+{
     (void)va_arg(args, Users_Catalog);
     (void)va_arg(args, Drivers_Catalog);
     Rides_Catalog rides_catalog = va_arg(args, Rides_Catalog);
@@ -140,34 +153,56 @@ void print_q6(FILE *output_file, char **fields, va_list args) {
 
     output = get_q6(city, start_date, end_date, rides_catalog);
 
-    if (output) {
+    if (output)
+    {
         fprintf(output_file, "%s\n", output);
         free(output);
     }
 }
 
-void print_q7(FILE *output_file, char **fields, va_list args) {
+void print_q7(FILE *output_file, char **fields, va_list args)
+{
+    va_list args_copy;
+    va_copy(args_copy, args);
+
+    (void)va_arg(args, Users_Catalog);
+    (void)va_arg(args, Drivers_Catalog);
+    Rides_Catalog rides_catalog = va_arg(args, Rides_Catalog);
+
+    int output_number = str_to_int(fields[1]);
+    char *city = fields[2];
+
+    sort_rides_by_city_and_driver_id(rides_catalog);
+
+    char *output = get_q7(output_number, city, args_copy);
+
+    if (output)
+    {
+        fprintf(output_file, "%s\n", output);
+        free(output);
+    }
+
+    va_end(args_copy);
+}
+
+void print_q8(FILE *output_file, char **fields, va_list args)
+{
     // Nothing to do
     (void)output_file;
     (void)fields;
     (void)args;
 }
 
-void print_q8(FILE *output_file, char **fields, va_list args) {
+void print_q9(FILE *output_file, char **fields, va_list args)
+{
     // Nothing to do
     (void)output_file;
     (void)fields;
     (void)args;
 }
 
-void print_q9(FILE *output_file, char **fields, va_list args) {
-    // Nothing to do
-    (void)output_file;
-    (void)fields;
-    (void)args;
-}
-
-void handle_query(FILE *output_file, char **fields, va_list args) {
+void handle_query(FILE *output_file, char **fields, va_list args)
+{
     void (*query_selector[9])(FILE *, char **, va_list) = {print_q1, print_q2, print_q3, print_q4, print_q5, print_q6, print_q7, print_q8, print_q9};
 
     int query = str_to_int(fields[0]);
